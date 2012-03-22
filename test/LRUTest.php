e5dfad239e395660583de4540c54ea7b74b2022c
rogeriopvl <rogeriopvl@gmail.com>
2012-03-22 10:02:21 +0000

    Wrote test for multiple gets. Its failing :(

--- test/LRUTest.php
+++ test/LRUTest.php
@@ -1,6 +1,6 @@
 <?php
 require_once('PHPUnit/Autoload.php');
-require_once(dirname(__FILE__).'/../LRU.php');
+require_once(dirname(__FILE__).'/../LRUCache.php');
 
 class LRUCacheTest extends PHPUnit_Framework_TestCase {
 
@@ -19,6 +19,20 @@ class LRUCacheTest extends PHPUnit_Framework_TestCase {
         $data = 'content for key1';
         $this->lru->put($key, $data);
         $this->assertEquals($this->lru->get($key), $data);
+    }
+
+    public function testMultipleGet() {
+        $key = 'key1';
+        $data = 'content for key1';
+        $key2 = 'key2';
+        $data2 = 'content for key2';
+
+        $this->lru->put($key, $data);
+        $this->lru->put($key2, $data2);
+
+        $this->assertEquals($this->lru->get($key), $data);
+        $this->assertEquals($this->lru->get($key2), $data2);
+        
     }
 
 }
