13b17474b9ef00eea6c7a3153d86aae64bd9957c
rogeriopvl <rogeriopvl@gmail.com>
2012-03-22 11:15:20 +0000

    Removed setUp() method. Cache is now created on every test method to avoid unpredicted results. All tests are currently passing.

--- test/LRUTest.php
+++ test/LRUTest.php
@@ -5,34 +5,35 @@ require_once(dirname(__FILE__).'/../LRUCache.php');
 class LRUCacheTest extends PHPUnit_Framework_TestCase {
 
     public function setUp() {
-        $this->lru = new LRUCache(1000);
     }
 
     public function testStartsEmpty() {
-        $this->assertNull($this->lru->get(1));
+        $lru = new LRUCache(1000);
+        $this->assertNull($lru->get(1));
     }
 
-    public function testPut() {}
-
     public function testGet() {
+        $lru = new LRUCache(1000);
         $key = 'key1';
         $data = 'content for key1';
-        $this->lru->put($key, $data);
-        $this->assertEquals($this->lru->get($key), $data);
+        $lru->put($key, $data);
+        $this->assertEquals($lru->get($key), $data);
     }
 
     public function testMultipleGet() {
+        $lru = new LRUCache(1000);
         $key = 'key1';
         $data = 'content for key1';
         $key2 = 'key2';
         $data2 = 'content for key2';
 
-        $this->lru->put($key, $data);
-        $this->lru->put($key2, $data2);
+        $lru->put($key, $data);
+        $lru->put($key2, $data2);
 
-        $this->assertEquals($this->lru->get($key), $data);
-        $this->assertEquals($this->lru->get($key2), $data2);
+        $this->assertEquals($lru->get($key), $data);
+        $this->assertEquals($lru->get($key2), $data2);
         
     }
 
+    public function testPut() {}
 }
